f9973194096ff3866c8cf19d355fe59344815837
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-02-05 22:15:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    self.new(students_array).tap { |student| @@all.push(self.new(student)) }
+    self.new(students_array).tap { |student| @@all << self.new(student) }
   end
 
   def add_student_attributes(attributes_hash)
